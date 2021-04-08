d06b0e8c016e5df8b7ce29d66e4f5f2e2ce01f7a
WebCoder2021 <82173844+WebCoder2021@users.noreply.github.com>
2021-04-08 23:14:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+    key.classList.remove("playing");
+    key.classList.toggle("playing");
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) =>
+    key.addEventListener("transitionend", removeTransition)
+);
+
+window.addEventListener("keydown", playSound);
